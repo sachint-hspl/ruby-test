5a18217d5a702e6ce17fb41083836c87a0ba7a69
Sachin Tawade <sachin.tawade@harbingergroup.com>
2025-07-10 15:28:49 +0530

    added changes for ranges

---
+++ ranges.rb
@@ -0,0 +1,53 @@
+puts "=====Ranges as Sequences====="
+# $, =", "   # Array value separator = not required
+range1 = (1..10).to_a
+range2 = ('bar'..'bav').to_a
+
+puts "#{range1}"
+puts "#{range2}"
+
+
+puts "======different functions======"
+
+
+digits = 0..9
+
+puts digits.include?(5)
+ret = digits.min
+puts "Min value is #{ret}"
+
+ret = digits.max
+puts "Max value is #{ret}"
+
+ret = digits.reject {|i| i < 5 }
+puts "Rejected values are #{ret}"
+
+digits.each do |digit|
+   puts "In Loop #{digit}"
+end
+
+
+puts "======Ranges as Conditions=========="
+score = 70
+result = case score
+   when 0..40 then "Fail"
+   when 41..60 then "Pass"
+   when 61..70 then "Pass with Merit"
+   when 71..100 then "Pass with Distinction"
+   else "Invalid Score"
+end
+
+puts result
+
+puts "======Ranges as Intervals=========="
+if ((1..10) === 5)
+   puts "5 lies in (1..10)"
+end
+
+if (('a'..'j') === 'c')
+   puts "c lies in ('a'..'j')"
+end
+
+if (('a'..'j') === 'z')
+   puts "z lies in ('a'..'j')"
+end
